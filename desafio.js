ee19d5ae30eb590279cb6cf723c6121bd0e2de11
jeniffermarques <genes.marques.silva@gmail.com>
2021-07-02 00:41:48 -0300

    teste realizado com sucesso

---
+++ desafio.js
@@ -0,0 +1,42 @@
+var dadosClientes = [
+    {
+    nome: "Lucas",
+    sobrenome: "da Silva",
+    idade: 21,
+    genero: "Masculino",
+    plano: "full",
+    carencia: false,
+    aquisicao: "12/01/2019"
+    },
+    {
+    nome: "Ana",
+    sobrenome: "Lima",
+    idade: 17,
+    genero: "Feminino",
+    plano: "medium",
+    carencia: false,
+    aquisicao: "17/03/2019"
+    },
+    {
+    nome: "Adriana",
+    sobrenome: "Menezes",
+    idade: 27,
+    genero: "Feminino",
+    plano: "full",
+    carencia: true,
+    aquisicao: "14/09/2020"
+    }
+]
+
+export function retornaLista(array){
+
+var listaIdades = []
+
+for(var i = 0; i < array.length; i++) {
+  listaIdades[i] = array[i]
+}
+
+return listaIdades.filter(pessoa => pessoa.idade >=18 && pessoa.idade <= 26)
+} 
+
+console.log(retornaLista(dadosClientes))
